1e879f1f5aefae7e7b0ca31afc9b95763ea300b6
QuakeOut <87473664+FMaksym@users.noreply.github.com>
2024-09-16 13:26:55 +0300

    Update UIView.cs

--- Assets/Source/UI/UIView.cs
+++ Assets/Source/UI/UIView.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class UIView : MonoBehaviour
 {
-    // Sets the slider value to the maximum firing force obtained from the bullet spavener.
+    // Sets the slider value to the maximum firing force obtained from the bullet spawner.
     public void ChangeSliderValue(UIModel model)
     {
         model.shootPowerSlider.value = model.bulletSpawner.GetMaxShootPower();
     }
     
-    // Sets the minimum and maximum values of the slider depending on the bullet spavener values.
+    // Sets the minimum and maximum values of the slider depending on the bullet spawner values.
     public void ChangeMinMaxShootPower(UIModel model)
     {
         model.shootPowerSlider.minValue = model.bulletSpawner.GetMinShootPower();
